6a417e8b081123cefc1e88415b849d992f5d4274
nayrow <paseresebastian0504@gmail.com>
2024-03-30 14:07:31 +0200

    final commit

---
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AttendeeResource;
+use App\Http\Traits\CanLoadRelationships;
+use App\Models\Attendee;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class AttendeeController extends Controller
+{
+    use CanLoadRelationships;
+    private array $relations = ['user'];
+
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index','show','update']);
+        $this->authorizeResource(Attendee::class,'attendee');
+    }
+    public function index(Event $event)
+    {
+        $attendees = $this->loadRelationships($event->attendees()->latest());
+
+        return AttendeeResource::collection(
+            $attendees->paginate()
+        );
+    }
+    public function store(Request $request, Event $event)
+    {
+        $attendee = $this->loadRelationships($event->attendees()->create(['user_id'=>1]));
+
+        return new AttendeeResource($attendee);
+
+    }
+    public function show(Event $event,Attendee $attendee)
+    {
+        return new AttendeeResource($this->loadRelationships($attendee));
+    }
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+    public function destroy(Event $event,Attendee $attendee)
+    {
+        $this->authorize('delete-attendee',[$event,$attendee]);
+        $attendee->delete();
+
+        return response(status:204);
+    }
+}
